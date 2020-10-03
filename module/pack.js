1430054915d8c83d147f494a9680bf44a17b6935
Daniel <mineminecraftpe687@gmail.com>
2020-10-03 15:54:25 +1000

    Update pack.js

--- module/pack.js
+++ module/pack.js
@@ -1,12 +1,12 @@
 export function juiceBox() {
   // Fetch The Box
-  var modal = document.getElementById("box");
+  var modal = document.getElementById("jb-box");
 
   // Fetch the button
-  var btn = document.getElementById("btn");
+  var btn = document.getElementById("jb-btn");
 
   // Get the close button
-  var span = document.getElementsByClassName("close")[0];
+  var span = document.getElementsByClassName("jb-close")[0];
   
   // Open Box when button click
   btn.onclick = function() {
@@ -25,3 +25,7 @@ export function juiceBox() {
     }
   }
 }
+export function juiceBoxPopup(head, cont, foot){
+  document.head.innerHTML+='<!-- The Box --> <div id="jb-box" class="jb-box"> <!-- Box content --> <div class="jb-box-content"> <div class="jb-box-header"> <span class="jb-close">&times;</span> <h2>'+ head +'</h2> </div> <div class="jb-box-body"> <p>'+ cont +'</p> </div> <div class="jb-box-footer"> <h3>'+ foot +'</h3> </div> </div> </div>'
+}
+// <!-- The Box --> <div id="box" class="box"> <!-- Box content --> <div class="box-content"> <div class="box-header"> <span class="close">&times;</span> <h2>Hello!</h2> </div> <div class="box-body"> <p>This is a JuiceBox popup!</p> <p>:)</p> </div> <div class="box-footer"> <h3>© JuiceBox</h3> </div> </div> </div>
