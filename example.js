c93597424bb2ce5c0f9a174d1bf9f0d0df4c5b69
Daniel <mineminecraftpe687@gmail.com>
2020-10-03 16:30:29 +1000

    Create example.js

---
+++ example.js
@@ -0,0 +1,25 @@
+// Fetch The Box
+var modal = document.getElementById("box");
+
+// Fetch the button
+var btn = document.getElementById("btn");
+
+// Get the close button
+var span = document.getElementsByClassName("close")[0];
+
+// Open Box when button click
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// Close box when button2 clicked
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the box, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
